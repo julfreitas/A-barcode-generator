af8e6514770f82657124fd4ed5d245905d11706c
Júlia Freitas <juliafreitas@alu.ufc.br>
2024-02-08 20:24:07 -0300

    feat: Implementing Validator

---
+++ src/errors/error_types/http_unprocessable_entity.py
@@ -0,0 +1,10 @@
+
+
+class HttpUnprocessableEntityError(Exception):
+    
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.message = message
+        self.name = "UnprocessableEntity"
+        self.status_code = 422
+        
